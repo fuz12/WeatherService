e96b704e6f70249312fe83f96e80c51e73bc5587
Maxim <zhavnerko2015@yandex.ru>
2024-02-15 15:46:05 +0300

    json service in action

--- JsonService.cpp
+++ JsonService.cpp
@@ -2,18 +2,19 @@
 
 Weather JsonService::getWeather(string fileName) {
     ifstream fin(fileName);
-    if (!fin)
-        throw exception("error");
+
+    if (!fin) throw exception("error");
 
     json j;
     j = json::parse(fin);
+
     string city = j["name"];
     double lat = j["coord"]["lat"];
     double lon = j["coord"]["lon"];
     double temperature = j["main"]["temp"];
-    string weather = j["weather"]["main"];
+    string weather = j["weather"][0]["description"];
     double windSpeed = j["wind"]["speed"];
     int clouds = j["clouds"]["all"];
-    return Weather(city, lat, lon, temperature, weather, windSpeed, clouds);
 
+    return Weather(city, lat, lon, temperature, weather, windSpeed, clouds);
 }
