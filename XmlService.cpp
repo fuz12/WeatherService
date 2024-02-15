1a46c789eb896f10170dace57641ef3565dacf6e
Maxim <zhavnerko2015@yandex.ru>
2024-02-15 18:37:33 +0300

    bug fixes

--- XmlService.cpp
+++ XmlService.cpp
@@ -9,8 +9,8 @@ Weather XmlService::getWeather(string fileName) {
     xml_node node = doc.child("current");
 
     string city = node.child("city").attribute("name").as_string();
-    double lat = node.child("city").child("coord").attribute("lat").as_int();
-    double lon = node.child("city").child("coord").attribute("lon").as_int();
+    double lat = node.child("city").child("coord").attribute("lat").as_double();
+    double lon = node.child("city").child("coord").attribute("lon").as_double();
     double temperature = node.child("temperature").attribute("value").as_double();
     string weather = node.child("weather").attribute("value").as_string();
     double windSpeed = node.child("wind").child("speed").attribute("value").as_double();
--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@
 #include "JsonService.hpp"
 #include "XmlService.hpp"
 
-int main()
-{
+void enableUtf8() {
     std::locale::global(std::locale{ ".UTF-8" });
     std::cout.imbue(std::locale(""));
+}
+
+void disableUtf8() {
+    setlocale(LC_ALL, "C");
+}
+
+int main()
+{
+    enableUtf8();
 
     JsonService js;
     Weather weather = js.getWeather("json_weather.json");
@@ -15,6 +23,8 @@ int main()
     cout << endl;
 
     XmlService xs;
+    disableUtf8();
     Weather w = xs.getWeather("xml_weather.xml");
+    enableUtf8();
     w.Print();
 }
